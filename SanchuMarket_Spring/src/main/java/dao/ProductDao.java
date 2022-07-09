5fbb1557abe3c6a0c8cc2e76f657caf323f9b015
AJin <dkwlsdl8@hanmail.net>
2022-07-09 19:52:32 +0900

    DAO:imageDaoImpl부터수정하세요

---
+++ SanchuMarket_Spring/src/main/java/dao/ProductDao.java
@@ -0,0 +1,49 @@
+package dao;
+
+import java.util.List;
+import java.util.Map;
+
+import vo.ProductVo;
+
+public interface ProductDao {
+
+
+	//상품 전체조회
+	public List<ProductVo> selectList(); 
+
+		
+	// 카테고리별 조회
+	public List<ProductVo> selectList(String c_idx);
+		
+	//u_idx 유저정보 넘김
+	public List<ProductVo> selectList(int u_idx);
+	
+	
+	//상품정보 넘김
+	public ProductVo selectList2(int p_idx);
+	
+
+	//최근 상품 조회(mainpage_admin)
+	public List<ProductVo> selectRecentList();
+		
+	
+	public int selectTodayCount();
+		
+	
+	// 상품등록 메서드
+	public int insert(ProductVo vo);
+	
+	
+	//상품 1개 조회 메서드
+	public ProductVo selectOne(int p_idx);
+		
+	//상품검색
+	public List<ProductVo> selectList(Map map);
+
+
+
+
+	
+	
+	
+}
