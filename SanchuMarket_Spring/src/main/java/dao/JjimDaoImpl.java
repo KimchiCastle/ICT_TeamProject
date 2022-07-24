6b7c13bf7762922a6eabeab4f4a6fd82a4684f66
chiseong kim <dlapdlf1013@gmail.com>
2022-07-24 16:58:05 +0900

    상품 찜기능 대략적으로 구현, 상품수정 가능

--- SanchuMarket_Spring/src/main/java/dao/JjimDaoImpl.java
+++ SanchuMarket_Spring/src/main/java/dao/JjimDaoImpl.java
@@ -17,15 +17,21 @@ public class JjimDaoImpl implements JjimDao {
 	}
 
 	@Override
-	public JjimVo selectOne() {
+	public JjimVo selectOne(Map map) {
 		// TODO Auto-generated method stub
-		return null;
+		return sqlSession.selectOne("jjim.jjim_select", map);
 	}
 
 	@Override
 	public int insert(JjimVo vo) {
 		// TODO Auto-generated method stub
 		return sqlSession.insert("jjim.jjim_insert",vo);
+	}
+
+	@Override
+	public int delete(JjimVo vo) {
+		// TODO Auto-generated method stub
+		return sqlSession.delete("jjim.jjim_delete", vo);
 	}
 
 }
