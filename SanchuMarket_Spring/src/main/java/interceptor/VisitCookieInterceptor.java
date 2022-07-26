1ba2f2533daee8663ae6bb0f448cef5f162b67e2
AJin <dkwlsdl8@hanmail.net>
2022-07-26 15:29:45 +0900

    에러수정

--- SanchuMarket_Spring/src/main/java/interceptor/VisitCookieInterceptor.java
+++ SanchuMarket_Spring/src/main/java/interceptor/VisitCookieInterceptor.java
@@ -1,24 +1,37 @@
 package interceptor;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import vo.UserVo;
-
-public class VisitCookieInterceptor implements HandlerInterceptor{
+public class VisitCookieInterceptor extends HandlerInterceptorAdapter{
 
 	private static final Logger logger = LoggerFactory.getLogger(AdminInterceptor.class);
 	 
 	@Override
 	    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 	            throws Exception {
+		
+			int cookieCnt;
+			
+			Cookie [] visitCookieArr = request.getCookies();
+			Cookie visitCookie = null;
+			
+			//브라우저에서 전달된 모든 쿠키를 검사하고
+			if(visitCookieArr != null) {
+				for(int i=0; i<visitCookieArr.length; i++) {
+					//그 중 방문자수에 필요한 쿠키만 추출해서 저장한다.
+					visitCookie = visitCookieArr[i];
+					break;
+				}
+			}
+		
 		 
-	        return true;
+	        return super.preHandle(request, response, handler);
 	        
 	    }
 }
